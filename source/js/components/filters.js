d7ebb7fe1ed2bb2a952d5799a86b865612e63062
Roman <romanshliakhov@gmail.com>
2023-08-24 17:21:25 +0300

    start fixes

--- source/js/components/filters.js
+++ source/js/components/filters.js
@@ -1,143 +1,146 @@
 document.addEventListener('DOMContentLoaded', function () {
   const categories = document.querySelector('.categories');
-  const categoriesTrigger = categories.querySelector('.categories__head');
-  const categoriesList = categories.querySelector('.categories__body');
-  const tagsParrent = document.querySelector('.categories__choosed');
-  const tagsHead = document.querySelector('.categories__head-current');
-  const allTagsReset = document.querySelector('.categories__btn-reset');
 
-  function renderPlaceholder() {
-    if (tagsHead) {
-      tagsHead.innerText = tagsHead.getAttribute('data-placeholder');
-    }
-  }
+  if(categories) {
+    const categoriesTrigger = categories.querySelector('.categories__head');
+    const categoriesList = categories.querySelector('.categories__body');
+    const tagsParrent = document.querySelector('.categories__choosed');
+    const tagsHead = document.querySelector('.categories__head-current');
+    const allTagsReset = document.querySelector('.categories__btn-reset');
 
-  renderPlaceholder();
-
-  function categoriesActiveClass(toggleElement, targetElement, activeClass = 'active') {
-    document.addEventListener("click", function (event) {
-      if (!targetElement.contains(event.target) && event.target !== toggleElement) {
-        targetElement.classList.remove(activeClass);
-      } else if (event.target === toggleElement) {
-        targetElement.classList.toggle(activeClass);
+    function renderPlaceholder() {
+      if (tagsHead) {
+        tagsHead.innerText = tagsHead.getAttribute('data-placeholder');
       }
-    });
-  }
+    }
 
-  categoriesActiveClass(categoriesTrigger, categoriesList);
+    renderPlaceholder();
 
-  const categoryCheckbox = categories.querySelectorAll('.categories__checkbox');
-  const inputData = [];
-
-  categoryCheckbox.forEach(function (item) {
-    item.addEventListener('click', function (e) {
-      checkedHandler(this);
-    })
-  });
-
-  function checkedHandler(input) {
-    const inputValue = input.value;
-    const inputName = input.getAttribute('data-name');
-    const inputIndex = inputData.findIndex(obj => obj.value === inputValue);
-
-    if (inputIndex !== -1) {
-      inputData.splice(inputIndex, 1);
-    } else {
-      inputData.push({
-        value: inputValue,
-        name: inputName
+    function categoriesActiveClass(toggleElement, targetElement, activeClass = 'active') {
+      document.addEventListener("click", function (event) {
+        if (!targetElement.contains(event.target) && event.target !== toggleElement) {
+          targetElement.classList.remove(activeClass);
+        } else if (event.target === toggleElement) {
+          targetElement.classList.toggle(activeClass);
+        }
       });
     }
 
-    if (inputData.length === 0) {
-      allTagsReset.parentNode.style.display = 'none';
-    } else {
-      allTagsReset.parentNode.style.display = 'flex';
-    }
+    categoriesActiveClass(categoriesTrigger, categoriesList);
 
-    createTagList(tagsParrent, inputData);
-    removeAllTags(allTagsReset, inputData);
-    selectPlaceholder(inputData, tagsHead);
-  }
+    const categoryCheckbox = categories.querySelectorAll('.categories__checkbox');
+    const inputData = [];
 
-  function createTagList(parent, data) {
-    parent.innerHTML = '';
-
-    data.forEach(obj => {
-      const markup = `
-        <li class="categories__choose" data-value="${obj.value}">
-          <span class="categories__choose-name">${obj.name}</span>
-          <span class="categories__choose-delete" data-id="${obj.value}">
-            <svg width='24' height='24'>
-              <use href='img/sprite/sprite.svg#close'></use>
-            </svg>
-          </span>
-        </li>
-      `;
-
-      parent.insertAdjacentHTML('afterbegin', markup);
+    categoryCheckbox.forEach(function (item) {
+      item.addEventListener('click', function (e) {
+        checkedHandler(this);
+      })
     });
 
-    removeTag(parent);
-  }
+    function checkedHandler(input) {
+      const inputValue = input.value;
+      const inputName = input.getAttribute('data-name');
+      const inputIndex = inputData.findIndex(obj => obj.value === inputValue);
 
-  function removeTag(parent) {
-    const removeTagBtns = parent.querySelectorAll('.categories__choose-delete');
+      if (inputIndex !== -1) {
+        inputData.splice(inputIndex, 1);
+      } else {
+        inputData.push({
+          value: inputValue,
+          name: inputName
+        });
+      }
 
-    removeTagBtns.forEach(btn => {
+      if (inputData.length === 0) {
+        allTagsReset.parentNode.style.display = 'none';
+      } else {
+        allTagsReset.parentNode.style.display = 'flex';
+      }
+
+      createTagList(tagsParrent, inputData);
+      removeAllTags(allTagsReset, inputData);
+      selectPlaceholder(inputData, tagsHead);
+    }
+
+    function createTagList(parent, data) {
+      parent.innerHTML = '';
+
+      data.forEach(obj => {
+        const markup = `
+          <li class="categories__choose" data-value="${obj.value}">
+            <span class="categories__choose-name">${obj.name}</span>
+            <span class="categories__choose-delete" data-id="${obj.value}">
+              <svg width='24' height='24'>
+                <use href='img/sprite/sprite.svg#close'></use>
+              </svg>
+            </span>
+          </li>
+        `;
+
+        parent.insertAdjacentHTML('afterbegin', markup);
+      });
+
+      removeTag(parent);
+    }
+
+    function removeTag(parent) {
+      const removeTagBtns = parent.querySelectorAll('.categories__choose-delete');
+
+      removeTagBtns.forEach(btn => {
+        btn.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          const currentBtnId = this.getAttribute('data-id');
+          const inputIndex = inputData.findIndex(obj => obj.value === currentBtnId);
+
+          if (inputIndex !== -1) {
+            inputData.splice(inputIndex, 1);
+            document.querySelector(`.categories__checkbox[value='${currentBtnId}']`).checked = false;
+          }
+
+          if (inputData.length === 0) {
+            allTagsReset.style.display = 'none';
+            allTagsReset.parentNode.style.display = 'none';
+          }
+
+          createTagList(tagsParrent, inputData);
+          selectPlaceholder(inputData, tagsHead);
+        })
+      })
+    }
+
+    function removeAllTags(btn, arr) {
+      if (arr.length > 0) {
+        btn.style.display = 'flex';
+        btn.parentNode.style.display = 'flex';
+      } else {
+        btn.style.display = 'none';
+        btn.parentNode.style.display = 'none';
+      }
+
       btn.addEventListener('click', function (e) {
         e.preventDefault();
+        arr = [];
+        createTagList(tagsParrent, arr);
+        renderPlaceholder();
 
-        const currentBtnId = this.getAttribute('data-id');
-        const inputIndex = inputData.findIndex(obj => obj.value === currentBtnId);
+        categoryCheckbox.forEach(function (item) {
+          item.checked = false;
+        });
 
-        if (inputIndex !== -1) {
-          inputData.splice(inputIndex, 1);
-          document.querySelector(`.categories__checkbox[value='${currentBtnId}']`).checked = false;
-        }
-
-        if (inputData.length === 0) {
-          allTagsReset.style.display = 'none';
-          allTagsReset.parentNode.style.display = 'none';
-        }
-
-        createTagList(tagsParrent, inputData);
-        selectPlaceholder(inputData, tagsHead);
+        btn.style.display = 'none';
+        btn.parentNode.style.display = 'none';
       })
-    })
-  }
-
-  function removeAllTags(btn, arr) {
-    if (arr.length > 0) {
-      btn.style.display = 'flex';
-      btn.parentNode.style.display = 'flex';
-    } else {
-      btn.style.display = 'none';
-      btn.parentNode.style.display = 'none';
     }
 
-    btn.addEventListener('click', function (e) {
-      e.preventDefault();
-      arr = [];
-      createTagList(tagsParrent, arr);
-      renderPlaceholder();
+    function selectPlaceholder(arr, currentBox) {
+      const dataContent = currentBox.getAttribute('data-content');
+      const tagCounter = arr.length;
+      const currentBoxText = currentBox.getAttribute('data-placeholder');
 
-      categoryCheckbox.forEach(function (item) {
-        item.checked = false;
-      });
-
-      btn.style.display = 'none';
-      btn.parentNode.style.display = 'none';
-    })
-  }
-
-  function selectPlaceholder(arr, currentBox) {
-    const dataContent = currentBox.getAttribute('data-content');
-    const tagCounter = arr.length;
-    const currentBoxText = currentBox.getAttribute('data-placeholder');
-
-    tagCounter > 0 ?
-      currentBox.innerText = `${tagCounter} ${dataContent}` :
-      currentBox.innerHTML = currentBoxText;
+      tagCounter > 0 ?
+        currentBox.innerText = `${tagCounter} ${dataContent}` :
+        currentBox.innerHTML = currentBoxText;
+    }
   }
 });
--- source/js/components/filtersSearch.js
+++ source/js/components/filtersSearch.js
@@ -1,74 +1,4 @@
-// // Get Input Filter Element
-// const filterInput = document.getElementById('filterInput');
-
-// filterInput.addEventListener('keyup', filterNames);
-
-// function filterNames(){
-//   // Get value of input
-//   let filterValue = filterInput.value.toUpperCase();
-
-//   // Get name of UL
-//   let ul = document.querySelector('.categories__options');
-
-//   // Get items
-//   let li = ul.querySelectorAll('.categories__checkbox');
-
-//   li.forEach(item => {
-//     let name = item.getAttribute('data-name');
-
-//     for( let i = 0; i < li.lenght; i++ ) {
-//       let a = li[i].getElementByTagName('input')[0];
-
-//       // if matched
-//       if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
-//         li[i].style.display = '';
-//       } else {
-//         li[i].style.display = 'none';
-//       }
-//     }
-//   })
-
-//   // Loop throught collections
-
-
-//   console.log(filterValue)
-// }
-
-
-// document.querySelector('#filterInput').oninput = function(){
-//   let valueInput = this.value.trim();
-
-//   const categoriesItems = document.querySelectorAll('.categories__checkbox');
-
-//   categoriesItems.forEach(item => {
-//     const name = item.getAttribute('data-name');
-
-//     console.log(name);
-//   })
-
-
-//   // // console.log(categoriesItems);
-
-//   // if ( valueInput != '') {
-//   //   // console.log(valueInput);
-
-//   //   categoriesItems.forEach(function(elem){
-//   //     // const name = elem.getAttribute('data-name');
-
-//   //     if(elem.getAttribute('data-name') == -1) {
-//   //       elem.classList.add('hide');
-//   //     } else {
-//   //       elem.classList.remove('hide');
-//   //     }
-//   //   })
-//   // }
-
-
-//   // console.log(categoriesItems);
-// }
-
-
-const inputId = 'filterInput';
+const inputId = document.getElementById('filterInput');
 const itemsData = '.categories__option';
 const dataName = 'data-name'
 let displaySet = false;
@@ -79,23 +9,26 @@ function getDisplayType(element) {
 	return elementStyle.display;
 }
 
-document.getElementById(inputId).onkeyup = function() {
-	var searchVal = this.value.toLowerCase();
-	var filterItems = document.querySelectorAll(itemsData);
+inputId && inputId.addEventListener('keyup', function() {
+  var searchVal = this.value.toLowerCase();
+  var filterItems = document.querySelectorAll(itemsData);
 
-	for(var i = 0; i < filterItems.length; i++) {
-		if (!displaySet) {
-			displayArr.push(getDisplayType(filterItems[i]));
-		}
+  for(var i = 0; i < filterItems.length; i++) {
+    if (!displaySet) {
+      displayArr.push(getDisplayType(filterItems[i]));
+    }
 
-		filterItems[i].style.display = 'none';
+    filterItems[i].style.display = 'none';
 
     const innerData = filterItems[i].querySelector(`[${dataName}]`);
 
-		if(innerData.getAttribute(dataName).indexOf(searchVal) >= 0) {
-			filterItems[i].style.display = displayArr[i];
-		}
-	}
+    if(innerData.getAttribute(dataName).indexOf(searchVal) >= 0) {
+      filterItems[i].style.display = displayArr[i];
+    }
+  }
 
-	displaySet = true;
-}
+  displaySet = true;
+})
+
+
+
